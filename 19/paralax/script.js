79bac9ad0c2254a0ee85f82b3f3304556db120f5
Matúš Marcin <matus.marcin@gmail.com>
2016-06-07 19:29:20 +0200

    Update script.js

--- 19/paralax/script.js
+++ 19/paralax/script.js
@@ -2,7 +2,19 @@
 
 	$(window).scroll( function() {
 		// console.log($(this).scrollTop());
-		$('.pozadie .obrazok').css('transform', 'translateY(-' + $(this).scrollTop()/3 + 'px)');
+
+		// ak zoskrolujem o 200px dole:
+		// .pozadie .obrazok { transform: translate3d(0, -100px, 0); }
+		$('.pozadie .obrazok').css('transform', 'translate3d(0, -' + $(this).scrollTop()/2 + 'px, 0)');
+
+		/** nejake napady, ako vyriesit velku bielu medzeru */
+
+		// 1. priblizovat obsah pod .pozadie zapornym margin-bottom
+		// $('.pozadie').css('margin-bottom', '-' + $(this).scrollTop()/3 + 'px');
+		
+		// 2. zmensovat .pozadie
+		// v kombinacii s overflow: hidden na .pozadie
+		$('.pozadie').css('height', 500-$(this).scrollTop()/2+'px');
 	});
 
 })(jQuery);
